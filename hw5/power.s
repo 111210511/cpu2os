0d49039372099174ac98ead9fbf7da7005680aa4
111210511 <162658490+111210511@users.noreply.github.com>
2025-05-24 12:00:46 +0800

    Create power.s

---
+++ hw5/power.s
@@ -0,0 +1,55 @@
+.file	"power.c"
+	.text
+	.globl	power
+	.type	power, @function
+power:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	$1, -12(%rbp)
+	movl	$0, -16(%rbp)
+	jmp	.L2
+.L3:
+	movl	-12(%rbp), %eax
+	imull	-4(%rbp), %eax
+	movl	%eax, -12(%rbp)
+	addl	$1, -16(%rbp)
+.L2:
+	movl	-16(%rbp), %eax
+	cmpl	-8(%rbp), %eax
+	jl	.L3
+	movl	-12(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	power, .-power
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	movl	$3, %esi
+	movl	$2, %edi
+	call	power
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 13.2.0-4ubuntu3) 13.2.0"
+	.section	.note.GNU-stack,"",@progbits
